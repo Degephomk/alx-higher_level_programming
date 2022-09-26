70e4dbb849f82f1db195cfbc2355024859511ad0
Degephomk <degiatb.k@gmail.com>
2022-09-26 05:44:16 -0700

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for a in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(a), end='')
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
