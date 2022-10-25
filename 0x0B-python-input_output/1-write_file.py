7d51ff3df8526a51679ede6da9ab8d7d4da43bac
Degephomk <degiatb.k@gmail.com>
2022-10-25 03:47:50 -0700

    adding

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
-"""This module is for number_of_lines."""
 
-
-def number_of_lines(filename=""):
-    """Returns the number of lines of a text file."""
-    lines = 0
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
-            lines += 1
-        return lines
+def write_file(filename="", text=""):
+    with open(filename, "w", encoding="UTF-8") as f:
+        return f.write(text)
