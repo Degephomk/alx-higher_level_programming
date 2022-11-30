fc94f1f39a5d5a24a2cb0531523f77f948ce87de
Degephomk <degiatb.k@gmail.com>
2022-11-30 05:20:36 -0800

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,3 @@
 -- Write a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
-CREATE DATABASE hbtn_0d_usa IF NOT EXISTS;
-    CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
-    PRIMARY KEY(`id`),
-    `id`   INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(256) NOT NULL
-);
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE, name VARCHAR(256));
