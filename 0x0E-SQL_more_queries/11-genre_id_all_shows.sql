120677cdbd59cefb02ee4c8e88ca2ef8d8b15244
Degephomk <degiatb.k@gmail.com>
2022-11-30 05:51:17 -0800

    11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,10 +1,6 @@
 --Write a script that lists all shows contained in the database hbtn_0d_tvshows
-SELECT
-    tv_shows.title,
-    tv_show_genres.genre_id
-FROM
-    tv_shows
-    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY
-    tv_shows.title,
-    tv_show_genres.genre_id;
+SELECT t.`title`, g.`genre_id`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS g
+       ON t.`id` = g.`show_id`
+ ORDER BY t.`title`, g.`genre_id`;
