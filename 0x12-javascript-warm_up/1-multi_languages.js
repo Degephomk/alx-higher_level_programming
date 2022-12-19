76a9e867649376f95952cd3861ca113664a3fc4c
Degephomk <degiatb.k@gmail.com>
2022-12-19 04:22:09 -0800

    add task1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,3 @@
 #!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavascript is amazing');  // print three lines in one
+// prints 3 lines
+console.log('C is fun\nPython is cool\nJavascript is amazing');
