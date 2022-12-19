3a6ccddf2a822eb8cc6fc444d1407bbeb1c1c892
Degephomk <degiatb.k@gmail.com>
2022-12-19 05:48:45 -0800

    add task3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const Argv = process.argv.length;
+if (Argv < 3) {
+	  console.log('No argument');
+} else if (Argv === 3) {
+	  console.log(Argv);
+} else {
+	  console.log(Argv);
+}
