5aa742d889953fb99ca16140f791f4b70cbafc1e
Degephomk <degiatb.k@gmail.com>
2022-12-19 07:56:14 -0800

    add

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const x = parseInt(process.argv[2]);
+let i, j;
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < x; i++) {
+    for (j = 0; j < x; j++) {
+      process.stdout.write('x');
+    }
+    console.log();
+  }
+}
