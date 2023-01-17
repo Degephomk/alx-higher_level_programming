077c71c470475bb61e39526a27a5caa5efe8c95e
Degephomk <degiatb.k@gmail.com>
2023-01-17 04:36:36 -0800

    adding

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""
-A python Script that takes in a URL and an email address, sends a POST request to the
-passed URL with the email as a parameter, and finally displays the body
-of the response.
+"""A python script that takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter, and finally displays
+the body of the response.
 """
 import requests
-from sys import argv
+import sys
 
-
-if __name__ == "__main__":
-    url = argv[1]
-    value = {"email": argv[2]}
-    req = requests.post(url, data=value)
-
-    print(req.text)
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    r = requests.post(url, data={'email': email})
+    print(r.text)
