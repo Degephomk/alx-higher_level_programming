2a6dfe5d5271e6e578a72e9a0ea78b300c7c49a3
Degephomk <degiatb.k@gmail.com>
2023-01-17 03:36:18 -0800

    adding

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
-a Python script that fetches https://alx-intranet.hbtn.io/status
-"""
+""" a Python script that fetches 
+https://alx-intranet.hbtn.io/status"""
+
 if __name__ == '__main__':
     import urllib.request
 
